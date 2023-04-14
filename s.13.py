bbc88839edb6514b512ceea3219fe35146678192
Alessandro-Demarco <alessandroilprgramatore8@gmail.com>
2023-04-14 17:38:05 +0200

    segsg

---
+++ s.13.py
@@ -0,0 +1,12 @@
+l1=["aple","banana","cherry","date"]
+l2=["banana","date","flag","grape"]
+d={}
+for x in l1:
+    if x not in l2:
+       print(x)
+for x in l2:
+    if x not in l1:
+       print(x)
+sl1=set(l1)
+sl2=set(l2)
+print(sl1.union(sl2))
